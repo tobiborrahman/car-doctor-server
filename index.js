b0a352d76ab76ea35e2e29d7ed779e06c69017b3
TobiborRahman <r.tobibor16@gmail.com>
2023-05-16 13:41:47 +0600

    env file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -19,10 +20,30 @@ const client = new MongoClient(uri, {
 	},
 });
 
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: 'unauthorized access' });
+	}
+
+	const token = authorization.split(' ')[1];
+	jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
+		if (error) {
+			return res
+				.status(401)
+				.send({ error: true, message: 'unauthorized access' });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
+
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
 		// Send a ping to confirm a successful connection
 
 		const carCollection = client.db('carDoctor').collection('services');
@@ -48,8 +69,16 @@ async function run() {
 		});
 
 		// Bookings
-		app.get('/bookings', async (req, res) => {
-			console.log(req.query.email);
+		app.get('/bookings', verifyJWT, async (req, res) => {
+			// console.log(req.query.email);
+			const decoded = req.decoded;
+			console.log('came back after verify', decoded);
+
+			if (decoded.email !== req.query.email) {
+				return res
+					.status(403)
+					.send({ error: 1, message: 'forbidden access' });
+			}
 			let query = {};
 			if (req.query?.email) {
 				query = { email: req.query.email };
@@ -63,6 +92,15 @@ async function run() {
 			console.log(booking);
 			const result = await bookingsCollection.insertOne(booking);
 			res.send(result);
+		});
+
+		app.post('/jwt', (req, res) => {
+			const user = req.body;
+			console.log(user);
+			const token = jwt.sign(user, process.env.JWT_TOKEN_SECRET, {
+				expiresIn: '1h',
+			});
+			res.send({ token });
 		});
 
 		app.patch('/bookings/:id', async (req, res) => {
